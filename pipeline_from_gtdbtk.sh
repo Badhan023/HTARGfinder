c39a56ad9fd2fb11343f18a04d526d4c3728cf28
Badhan Das <badhan.2192@gmail.com>
2021-09-13 18:37:57 -0400

    Update pipeline_from_gtdbtk.sh

--- pipeline_from_gtdbtk.sh
+++ pipeline_from_gtdbtk.sh
@@ -7,16 +7,16 @@
 
 echo $1     ##prefix of files
 
-cd ${1}_metabat_bins_dir
+cd ${1}_bins_dir
 var=( $(ls) )
 echo ${var[0]}
 
 ##gtdb-toolkit
-gtdbtk classify_wf --genome_dir ${1}_metabat_bins_dir -x $extension --out_dir ${1}_gtdbtk_out --cpus 32
+gtdbtk classify_wf --genome_dir ${1}_bins_dir -x $extension --out_dir ${1}_gtdbtk_out --cpus 32
 echo "gtdb-tookit done"
 
 ##MetaCHIP
-MetaCHIP PI -p $1 -r c -t 6 -i ${1}_metabat_bins_dir -x $extension -taxon ${1}_gtdbtk_out/gtdbtk.bac120.summary.tsv
+MetaCHIP PI -p $1 -r c -t 6 -i ${1}_bins_dir -x $extension -taxon ${1}_gtdbtk_out/gtdbtk.bac120.summary.tsv
 MetaCHIP BP -p $1 -r c -t 6
 echo "MetaCHIP done"
 
